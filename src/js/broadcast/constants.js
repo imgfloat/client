c90e8cf54e990c68759ec25c6aea04b3c18c1765
Andreas Krühlmann <andreas@kruhlmann.dev>
2026-01-13 14:51:28 +0100

    Move client back to server

--- src/js/broadcast/constants.js
+++
@@ -1,9 +0,0 @@
-export const TARGET_FPS = 60;
-export const MIN_FRAME_TIME = 1000 / TARGET_FPS;
-export const VISIBILITY_THRESHOLD = 0.01;
-
-export const AssetKind = Object.freeze({
-    AUDIO: "audio",
-    CODE: "code",
-    VISUAL: "visual",
-});
